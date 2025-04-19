6e486f2ee1583f15197623c800895fd49a6b0ccd
Roee Yaish <roeey@hotelmize.com>
2025-04-19 19:33:48 +0300

    New Update script URL after redeployment

--- api/submit.js
+++ api/submit.js
@@ -1,5 +1,13 @@
 export async function POST(req) {
-    const body = await req.json();
+    const body = await req.json()
+  
+    // בדוק אם body הוא מערך של אובייקטים עם user, gameId, pick
+    const isValid = Array.isArray(body) && body.length && body[0].user
+  
+    if (!isValid) {
+      console.error("Invalid body:", body)
+      return new Response("Invalid data", { status: 400 })
+    }
   
     const timestamp = new Date().toLocaleString("en-IL", {
       timeZone: "Asia/Jerusalem",
@@ -8,14 +16,14 @@ export async function POST(req) {
       day: "2-digit",
       hour: "2-digit",
       minute: "2-digit",
-    });
+    })
   
     const formatted = body.map((row) => [
       row.user,
       row.gameId,
       row.pick,
       timestamp,
-    ]);
+    ])
   
     const res = await fetch(
       "https://script.google.com/macros/s/AKfycbzBg4pfDsMtjhQaocAZ9n1UFOhNBqJ4Drz6MU_67F7EOgKRHCla1fzeQCZOcFIsiaE/exec",
@@ -26,8 +34,8 @@ export async function POST(req) {
         },
         body: JSON.stringify(formatted),
       }
-    );
+    )
   
-    return new Response("OK");
+    return new Response("OK")
   }
   
